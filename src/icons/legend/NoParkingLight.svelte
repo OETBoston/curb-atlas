24de3bbcebf4cb0d7a7ecd9aac1a5a1d71c73c8a
Alan McConchie <alan.mcconchie@gmail.com>
2026-02-19 16:11:51 -0800

    fix legend for dotted line

--- src/icons/legend/NoParkingLight.svelte
+++ src/icons/legend/NoParkingLight.svelte
@@ -1,4 +1,4 @@
 <svg width="100" height="20" viewBox="0 0 100 20" fill="none" xmlns="http://www.w3.org/2000/svg">
 <rect width="100" height="20" transform="matrix(1 0 0 -1 0 20)" fill="white"/>
-<path d="M10 10L52.2591 10L65.5482 10L77.7741 10L90 10" stroke="#F9B685" stroke-width="5" stroke-linecap="round"/>
+<path d="M10 10L96 10" stroke="#F9B685" stroke-width="5" stroke-linecap="round" stroke-dasharray="1 8"/>
 </svg>
